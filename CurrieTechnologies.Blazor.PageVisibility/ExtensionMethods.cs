ef05fb27f0879a5272b07b6d5bac0850d9d5408f
Michael J. Currie <michaeljcurrie136@gmail.com>
2019-06-08 15:19:50 -0500

    Add DI extension method

---
+++ CurrieTechnologies.Blazor.PageVisibility/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrieTechnologies.Blazor.PageVisibility
+{
+    public static class ExtensionMethods
+    {
+        public static IServiceCollection AddPageVisibiliy(this IServiceCollection services)
+        {
+            return services.AddSingleton<PageVisibilityService>();
+        }
+    }
+}
